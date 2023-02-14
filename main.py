a913c89db019c5ca8ebd99aa7cf23fa899c8ec0c
rezkidsl <rezkidsl97@gmail.com>
2023-02-15 01:21:54 +0700

    poop2

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ import json
 
 
 app = FastAPI(title='Rezki')
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*']
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 class PayloadECG(BaseModel):
